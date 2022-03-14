041d5ff03d685d251b3cf3f9991ed8f81e5ea4ae
sirussalari <sirus.salari@gmail.com>
2022-03-13 22:52:03 -0700

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let sum = null;
+    let range = null;
+    if (firstInt > secondInt) {
+        sum = secondInt;
+        range = firstInt;
+    }
+    else {
+        sum = firstInt;
+        range = secondInt;
+    }
+    for (let i = (sum + 1); i < (range + 1); i++) {
+        sum += i;
+    }
+    if (typeof firstInt !== 'number' || typeof secondInt !== 'number') {
+        return 'ERROR'
+    }
+    else if (firstInt > 0 && secondInt > 0) {
+        return sum;
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
