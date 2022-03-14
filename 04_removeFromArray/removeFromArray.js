ce33a9807ec2b458865ca7497d0fd40f0c5d9fe8
sirussalari <sirus.salari@gmail.com>
2022-03-13 22:26:13 -0700

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const args = Array.from(arguments);
+    const arr = args[0];
+    let update = [];
+    for (elem of arr) {
+        if (!args.includes(elem)) {
+            update.push(elem)
+        }
+    }
+    return update;
 };
 
 // Do not edit below this line
