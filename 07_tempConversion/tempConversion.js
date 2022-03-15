9608389fe5fe1cd3fba944ba909dec57a50111a6
sirussalari <sirus.salari@gmail.com>
2022-03-14 19:19:48 -0700

    Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let c = (5 * (tempF -32)) / 9;
+  const intC = Math.floor(c);
+  const decimals = c - intC;
+  const rounded = Math.round(10 * decimals);
+  const tenth = rounded / 10;
+  c = intC + tenth;
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let f = ((9 * tempC) + 160) / 5;
+  const intF = Math.floor(f);
+  const decimals = f - intF;
+  const rounded = Math.round(10 * decimals);
+  const tenth = rounded / 10;
+  f = intF + tenth;
+  return f;
 };
 
 // Do not edit below this line
