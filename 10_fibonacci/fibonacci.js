4e2f0556291fa3fb0b3a6eb99e6db03756351b20
sirussalari <sirus.salari@gmail.com>
2022-03-22 14:59:19 -0700

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    else {
+        let fib = [1,1];
+        for(let i = 0; i < (index - 2); i++) {
+        fib[fib.length] = fib[fib.length - 1] + fib[fib.length - 2];
+    }
+        return fib[fib.length - 1];
+    }
 };
 
 // Do not edit below this line
